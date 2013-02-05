4084b3e09ca1d3d49604a72548c9e53ee06ffb57
Naoto Mawatari <github.mawatari@gmail.com>
2013-02-05 09:11:52 +0900

    1-3. DebugKit, Migrations, TwitterBootstrap, Search プラグインの導入と初期設定

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -70,6 +70,7 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
+CakePlugin::load(array('TwitterBootstrap', 'Migrations', 'Search', 'DebugKit'));
 
 /**
  * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
